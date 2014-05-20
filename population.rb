926c2e53ca24674c41c6be1621718e5583a5e234
Chirantan Mitra <chirantan.mitra@gmail.com>
2014-05-20 10:53:27 +0530

    Rename file to match class name

--- population.rb
+++ population.rb
@@ -22,12 +22,12 @@ class Population
   def run
     create_routes
     tick until terminate?
+    routes.each { |route| puts route }
   end
 
   def tick
     @current_generation_run += 1
     routes.each { |route| route.mutate! if mutate? }
-    routes.each { |route| puts route }
     puts "Generation: #{@current_generation_run}"
   end
 
@@ -37,6 +37,6 @@ class Population
   private :mutate?
 
   def terminate?
-    current_generation_run > max_generation_runs
+    current_generation_run >= max_generation_runs
   end
 end
